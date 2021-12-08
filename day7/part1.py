73ee8612659a3c0dfbab59a81fd85c7eb0bc889f
u08120fh <feroz7hassan@gmail.com>
2021-12-08 02:04:40 +0000

    solution to part 1

---
+++ day7/part1.py
@@ -0,0 +1,15 @@
+
+with open("input.txt") as file:
+    crabs = [int(x) for x in file.read().strip().split(",")]
+
+crabs.sort()
+middle = crabs[len(crabs)//2]
+
+fuel = 0
+for pos in crabs:
+    if pos > middle:
+        fuel += pos-middle
+    elif pos < middle:
+        fuel += middle-pos
+
+print(fuel)
