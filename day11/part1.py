955b24a7d3b6a15044d46f095daa221fb7a82694
u08120fh <feroz7hassan@gmail.com>
2021-12-11 09:20:18 +0000

    solution to part 1

---
+++ day11/part1.py
@@ -0,0 +1,42 @@
+
+
+with open("input.txt") as file:
+    lines = [list(map(int, line.strip("\n"))) for line in file.readlines()]
+
+
+row = len(lines[0])
+col = len(lines)
+
+xc = [-1, -1, -1, 0, 1, 1, 1, 0]
+yc = [-1, 0, 1, 1, 1, 0, -1, -1]
+
+score = 0
+
+
+def flashed(r, c):
+    global score
+    score += 1
+    lines[r][c] = -1
+    for d in range(8):
+        rr = r + xc[d]
+        cc = c + yc[d]
+        if 0 <= rr < row and 0 <= cc < col and lines[rr][cc] != -1:
+            lines[rr][cc] += 1
+            if lines[rr][cc] >= 10:
+                flashed(rr, cc)
+
+
+for i in range(100):
+    for r in range(row):
+        for c in range(col):
+            lines[r][c] += 1
+    for r in range(row):
+        for c in range(col):
+            if lines[r][c] == 10:
+                flashed(r, c)
+    for r in range(row):
+        for c in range(col):
+            if lines[r][c] == -1:
+                lines[r][c] = 0
+
+print(score)
